3a226b8892d7aa8de04f1b291e84a533fee3a822
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-08-17 14:22:10 +0000

    submitting code-Thu Aug 17 2023 14:22:08 GMT+0000 (Coordinated Universal Time)

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -5,11 +5,11 @@ const Tabs = () =>{
 
     return (
         <div className="tabs">
-            <li onClick={()=>setCurrentTab('Tab 1')}>Tab 1</li>
-            <li onClick={()=>setCurrentTab('Tab 2')}>Tab 2</li>
-            <li onClick={()=>setCurrentTab('Tab 3')}>Tab 3</li>
+            <li onClick={()=>setCurrentTab('This is the content for Tab 1.')}>Tab 1</li>
+            <li onClick={()=>setCurrentTab('This is the content for Tab 2.')}>Tab 2</li>
+            <li onClick={()=>setCurrentTab('This is the content for Tab 3.')}>Tab 3</li>
             {currentTab && 
-                <p>This is the content for {currentTab}.</p>
+                <p>{currentTab}</p>
             } 
         </div>
     )
